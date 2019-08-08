2f65ad9d91cb8af99b9fdcc631e257e08dd369db
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-08-08 16:38:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name= (new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (breed_type)
+    @breed = breed_type
+  end
+
+  def breed
+    @breed
+  end
+end
